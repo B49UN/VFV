d9c21ee591d340fd77b3630553fabaf3d17c486d
bagun <sdwj1030@gmail.com>
2024-02-22 12:26:08 +0900

    version 0.1

--- env/vfvlib.py
+++ env/vfvlib.py
@@ -23,15 +23,26 @@ def ipt_gsheet(num):
     df = pd.DataFrame(worksheet.get_all_records())
 
     # Check how many sentences are analyzed
-    #count = 0
-    #number_of_sts = df.shape[0]
-    #for i in range(number_of_sts):
+    # count = 0
+    # number_of_sts = df.shape[0]
+    # for i in range(number_of_sts):
     #    if df.iloc[7, 5] == '':
     #        count += 1
 
     if num == 1:
         # print(f'<Information>\n\tTotal {number_of_sts} sentences, {number_of_sts - count} sentences POS-analyzed')
         return df.iloc[:, 0]
+
+
+instruction = "너는 영어를 잘 못하는 한국 학생들을 가르치는 영어 선생님이야. " \
+              + "네가 어떻게 답변해야 할지 그 규칙을 알려줄게. " \
+              + "1. 첫번째 줄은 분석 대상인 영어 문장을 <>안에 넣어서 보여줘. 첫번째 줄은 볼드체야." \
+              + "2. 두번째 줄부터는 문장 성분(주어, 목적어, 동사 등)별로 한 줄을 작성해. " \
+              + "3. 그 순서는 해당 영어 구절 - 문장 성분(주어, 목적어, 동사 등) - 한국어 해석 순이야." \
+              + "4. 문장이 몇형식인지(1형식, 2형식, 3형식 등)를 알려줘. 5. 가장 마지막 줄에는 문장 전체의 한국어 해석을 적어줘." \
+              + "6. 만약 문장에 구 또는 절이 있다면 그것을 규칙 2., 3.에 맞게 따로 분석해서 답변 가장 밑에 작성해. "\
+              + "답변은 markdown 형식으로 적어"
+
 
 def structure_analysis_detail(sentence):
     # prepare to use OpenAI API
@@ -45,13 +56,7 @@ def structure_analysis_detail(sentence):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system",
-             "content": "You are an English teacher who teaches Korean students who are not good at English. Let me "
-                        "choose the format of your answers. 1. The answers are made up of several lines. 2. The first "
-                        "line contains the English sentences to be analyzed in <>. 3. Write a line per commentary for "
-                        "one word in the original sentence from the second line. The format is the original words - "
-                        "sentence components(subject, object, etc.) - Part of Speech - Korean interpretation. 4. The "
-                        "last line is the Korean"
-                        "interpretation for the whole sentence."},
+             "content": instruction},
             {"role": "user", "content": "Analyze sentence:" + sentence}
         ]
     )
